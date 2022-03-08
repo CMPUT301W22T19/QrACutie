9293f445dbd78be5ff9464e0bc800c592536e855
s-gul <gul@ualberta.ca>
2022-03-08 14:13:24 -0700

    User login

---
+++ app/src/main/java/com/example/qracutie/Player.java
@@ -0,0 +1,24 @@
+package com.example.qracutie;
+
+import java.util.HashMap;
+
+public class Player {
+    /*
+    String playerQRCode;
+    String userName;
+    String email;
+    String phoneNumber;
+    int pointTotal;
+    String highestPointQRCode;
+    GameQRCode[] scannedCodes;
+    HashMap<String, Images> imagesHashMap = new HashMap<String, Images>();
+     */
+
+    String username;
+
+    public Player(String username) {
+        this.username = username;
+    }
+
+}
+
