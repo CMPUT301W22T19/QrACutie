00311c0ee9da58783169647870b000afa0edff3a
Noah Batiuk <batiuk@ualberta.ca>
2022-03-14 00:18:55 -0600

    database connected

--- app/src/main/java/com/example/qracutie/CommentsPage.java
+++ app/src/main/java/com/example/qracutie/CommentsPage.java
@@ -1,8 +1,11 @@
 package com.example.qracutie;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +16,24 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class CommentsPage extends AppCompatActivity {
 
@@ -38,6 +53,12 @@ public class CommentsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
+
+        // Get a top level reference to the collection
+        final CollectionReference collectionReference = db.collection("GameQRCodes/11/comments/");
 
         commentList = findViewById(R.id.comment_list);
 
@@ -72,16 +93,76 @@ public class CommentsPage extends AppCompatActivity {
             public void onClick(View view) {
 
                 // Retrieving the comment name and the province name from the EditText fields
-                final String commentName = addCommentEditText.getText().toString();
-                final String provinceName = addCommentEditText.getText().toString();
+                final String commentText = addCommentEditText.getText().toString();
 
-                HashMap<String, String> data = new HashMap<>();
 
-                if (commentName.length() > 0 && provinceName.length() > 0) {
+
+                // TEMP
+                final String userName = "batiuk";
+
+
+                // https://stackoverflow.com/questions/8654990/how-can-i-get-current-date-in-android
+                Date c = Calendar.getInstance().getTime();
+                System.out.println("Current time => " + c);
+
+                SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+                final String formattedDate = df.format(c);
+
+                // https://cloud.google.com/firestore/docs/manage-data/add-data#javaandroid
+                Map<String, Object> data = new HashMap<>();
+
+                if (commentText.length() > 0 && userName.length() > 0) {
 
                     // If there’s some data in the EditText field, then we create a new key-value pair.
-                    data.put("Province Name", provinceName);
+                    data.put("date", formattedDate);
+                    data.put("text", commentText);
+                    data.put("uid", userName);
+
                 }
+
+                // The set method sets a unique id for the document
+                collectionReference
+                        .document(commentText)
+                        .set(data)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                // These are a method which gets executed when the task is succeeded
+
+                                Log.d(TAG, "Data has been added successfully!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // These are a method which gets executed if there’s any problem
+                                Log.d(TAG, "Data could not be added!" + e.toString());
+                            }
+                        });
+
+                // Setting the fields to null so that user can add a new city
+                addCommentEditText.setText("");
+            }
+        });
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                    FirebaseFirestoreException error) {
+
+                // Clear the old list
+                commentDataList.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    Log.d(TAG, String.valueOf(doc.getData().get("date")));
+
+                    String text = (String) doc.getData().get("text");
+                    String date = (String) doc.getData().get("date");
+                    String uid = (String) doc.getData().get("uid");
+
+                    commentDataList.add(new Comment(text, uid, date)); // Adding the cities and provinces from FireStore
+                }
+                commentAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
             }
         });
     }
