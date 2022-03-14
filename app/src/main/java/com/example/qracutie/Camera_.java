852e3d22e9ca608a4d9a3a6e661249b06452b238
avj99 <avnishjadhav091@gmail.com>
2022-03-14 13:40:45 -0600

    Scanner works

--- app/src/main/java/com/example/qracutie/Camera_.java
+++ app/src/main/java/com/example/qracutie/Camera_.java
@@ -2,34 +2,9 @@ package com.example.qracutie;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.media.Image;
-import android.os.Bundle;
-import android.util.Log;
-import android.util.Size;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.mlkit.vision.barcode.BarcodeScanner;
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
-import com.google.mlkit.vision.barcode.BarcodeScanning;
-import com.google.mlkit.vision.barcode.common.Barcode;
-import com.google.mlkit.vision.common.InputImage;
-
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -37,33 +12,54 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
-import java.nio.ByteBuffer;
-import java.util.List;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Camera_ extends AppCompatActivity {
-    private ListenableFuture<ProcessCameraProvider> listenableFuture;
-    private PreviewView previewView;
-    private ExecutorService executor;
     private static final int PERMISSION_REQUEST_CAMERA = 0;
+    private PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    private Button qrCodeFoundButton;
+    private String qrCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        previewView= findViewById(R.id.PeekView);
-        listenableFuture= ProcessCameraProvider.getInstance(this);
+        previewView = findViewById(R.id.PeekView);
 
-        requestCamera() ;
+        qrCodeFoundButton = (Button) findViewById(R.id.qrCodeFoundButton);
+        qrCodeFoundButton.setVisibility(View.INVISIBLE);
+        qrCodeFoundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), qrCode, Toast.LENGTH_SHORT).show();
+                Log.i(MainActivity.class.getSimpleName(), "QR Code Found: " + qrCode);
+            }
+        });
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        requestCamera();
     }
+
     private void requestCamera() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            showCamera();
+            startCamera();
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+                ActivityCompat.requestPermissions(Camera_.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
             } else {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
             }
@@ -75,120 +71,55 @@ public class Camera_ extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CAMERA) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                showCamera();
+                startCamera();
             } else {
                 Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void showCamera() {
-        listenableFuture= ProcessCameraProvider.getInstance(this);
-        listenableFuture.addListener(()-> {
-            try{
-                ProcessCameraProvider processCameraProvider =listenableFuture.get();
-                cameraPreview(processCameraProvider);
-            }
-            catch(ExecutionException | InterruptedException i){
-                Toast.makeText(this, "There was an error starting the camera", Toast.LENGTH_SHORT).show();
+    private void startCamera() {
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindCameraPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                Toast.makeText(this, "Error starting camera " + e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }, ContextCompat.getMainExecutor(this));
     }
 
-    private void cameraPreview(@NonNull ProcessCameraProvider processCameraProvider) {
-        previewView.setImplementationMode(PreviewView.ImplementationMode.COMPATIBLE);
+    private void bindCameraPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        previewView.setImplementationMode(PreviewView.ImplementationMode.PERFORMANCE);
 
-        Preview.Builder previewBuilder = new Preview.Builder();
-        Preview previewCase = previewBuilder.build();
+        Preview preview = new Preview.Builder()
+                .build();
 
-        previewCase.setSurfaceProvider(previewView.getSurfaceProvider());
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
 
-        CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        BarcodeScannerOptions options =
-                new BarcodeScannerOptions.Builder()
-                        .setBarcodeFormats(
-                                Barcode.FORMAT_QR_CODE,
-                                Barcode.FORMAT_AZTEC)
-                        .build();
-
-        //processCameraProvider.unbindAll();
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder()
-                        // enable the following line if RGBA output is needed.
-                        //.setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
                         .setTargetResolution(new Size(1280, 720))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                         .build();
 
-        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new QRCodeImageAnalyzer(new QRCodeFoundListener() {
             @Override
-            public void analyze(@NonNull ImageProxy imageProxy) {
-                int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
-                ByteBuffer buffer = imageProxy.getPlanes()[0].getBuffer();
-                // insert your code here.
-                InputImage image = InputImage.fromByteBuffer(buffer,imageProxy.getWidth(),imageProxy.getHeight(),
-                        rotationDegrees,
-                        InputImage.IMAGE_FORMAT_NV21 // or IMAGE_FORMAT_YV12
-                );
-                BarcodeScanner scanner = BarcodeScanning.getClient();
-                Task<List<Barcode>> result = scanner.process(image)
-                        .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
-                            @Override
-                            public void onSuccess(List<Barcode> barcodes) {
-                                for (Barcode barcode: barcodes) {
-                                    Rect bounds = barcode.getBoundingBox();
-                                    Point[] corners = barcode.getCornerPoints();
-
-                                    String rawValue = barcode.getRawValue();
-
-                                    int valueType = barcode.getValueType();
-                                    // See API reference for complete list of supported types
-                                    switch (valueType) {
-                                        case Barcode.TYPE_URL:
-                                            String title = barcode.getUrl().getTitle();
-                                            String url = barcode.getUrl().getUrl();
-                                            break;
-                                    }
-                                }
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Task failed with an exception
-                                // ...
-                            }
-                        });
-
-                // after done, release the ImageProxy object
-                imageProxy.close();
+            public void onQRCodeFound(String _qrCode) {
+                qrCode = _qrCode;
+                qrCodeFoundButton.setVisibility(View.VISIBLE);
             }
-        });
 
-        processCameraProvider.unbindAll();
-        processCameraProvider.bindToLifecycle(this, cameraSelector, previewCase,imageAnalysis);
+            @Override
+            public void qrCodeNotFound() {
+                qrCodeFoundButton.setVisibility(View.INVISIBLE);
+            }
+        }));
 
-
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageAnalysis, preview);
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
