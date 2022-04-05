262cf0b3f25d8aa2a0bb7dea5cb01a41c0762977
Austin Tralnberg <atralnberg@gmail.com>
2022-04-05 12:36:48 -0600

    Prepopulated email and text feilds

--- app/src/main/java/com/example/qracutie/Account.java
+++ app/src/main/java/com/example/qracutie/Account.java
@@ -1,13 +1,23 @@
 package com.example.qracutie;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * The account activity allows a player to chang contact information associated with
@@ -33,6 +43,17 @@ public class Account extends AppCompatActivity {
         newEmail = findViewById(R.id.new_email);
         newPhonenumber = findViewById(R.id.new_phonenumber);
         done = findViewById(R.id.done);
+
+        // prepopulate email and phone number fields
+        db.collection("users").document(intent.getStringExtra("username")).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                String oldEmail = task.getResult().get("email").toString();
+                newEmail.setText(oldEmail);
+                String oldNumber = task.getResult().get("phoneNumber").toString();
+                newPhonenumber.setText(oldNumber);
+            }
+        });
 
        done.setOnClickListener(new View.OnClickListener() {
            @Override
