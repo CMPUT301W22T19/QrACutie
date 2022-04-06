cef6ec4717ab1037504424592a6249fb7c4ca1e5
Hetul-P <hetulspatel00@gmail.com>
2022-04-05 22:38:53 -0600

    Added intent testing for Owner's Page

--- app/src/androidTest/java/com/example/qracutie/OwnerAllPlayersTest.java
+++ app/src/androidTest/java/com/example/qracutie/OwnerAllPlayersTest.java
@@ -1,15 +1,19 @@
 package com.example.qracutie;
 
+import static android.view.View.GONE;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
@@ -19,14 +23,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.w3c.dom.Text;
 
-public class AccountTest {
+public class OwnerAllPlayersTest {
     private Solo solo;
 
     @Rule
     public ActivityTestRule<Account> rule =
-            new ActivityTestRule(Account.class, true, true){
+            new ActivityTestRule(OwnerAllPlayers.class, true, true){
                 @Override
                 protected Intent getActivityIntent (){
                     Intent intent = new Intent();
@@ -50,33 +53,24 @@ public class AccountTest {
      * checks if activity correctly switches when Back button is pressed
      */
     @Test
-    public void checkShareableQrActivitySwitch() {
+    public void checkChangeRecyclerView() {
         // Asserts that the current activity is the Account. Otherwise, show “Wrong Activity”
-        solo.assertCurrentActivity("Wrong Activity", Account.class);
+        solo.assertCurrentActivity("Wrong Activity", OwnerAllPlayers.class);
 
-        // click the back button
-        ImageButton button = (ImageButton) solo.getView(R.id.user_qr_button);
-        solo.clickOnView(button);
+        LinearLayout playerView = (LinearLayout) solo.getView(R.id.L_player);
+        LinearLayout qrView = (LinearLayout) solo.getView(R.id.L_Qr);
+        int playerVisibility = playerView.getVisibility();
+        int qrVisibility = qrView.getVisibility();
+        assertEquals(VISIBLE, playerVisibility);
+        assertEquals(GONE, qrVisibility);
 
-        // Asserts that the current activity switched to ShareableQrActivity. Otherwise, show “Wrong Activity”
-        solo.assertCurrentActivity("Wrong Activity", ShareableQrActivity.class);
-    }
+        androidx.appcompat.widget.AppCompatButton switchButton = (androidx.appcompat.widget.AppCompatButton) solo.getView(R.id.to_qr_list);
+        solo.clickOnView(switchButton);
 
-    @Test
-    public void checkDataInput(){
-        EditText emailText = (EditText) solo.getView(R.id.new_email);
-        emailText.setText("testEmail");
-
-        EditText phoneText = (EditText) solo.getView(R.id.new_phonenumber);
-        phoneText.setText("000000000");
-
-        Button done = (Button) solo.getView(R.id.done);
-        solo.clickOnView(done);
-        String newEmail = emailText.getText().toString();
-        String newPhone = phoneText.getText().toString();
-
-        assertEquals("testEmail", newEmail);
-        assertEquals("000000000", newPhone);
+        int newPlayerVisibility = playerView.getVisibility();
+        int newQrVisibility = qrView.getVisibility();
+        assertEquals(GONE, newPlayerVisibility);
+        assertEquals(VISIBLE, newQrVisibility);
     }
 
     @After
--- app/src/androidTest/java/com/example/qracutie/OwnerLoginTest.java
+++ app/src/androidTest/java/com/example/qracutie/OwnerLoginTest.java
@@ -1,14 +1,12 @@
 package com.example.qracutie;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -19,14 +17,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.w3c.dom.Text;
 
-public class AccountTest {
+public class OwnerLoginTest {
     private Solo solo;
 
     @Rule
     public ActivityTestRule<Account> rule =
-            new ActivityTestRule(Account.class, true, true){
+            new ActivityTestRule(OwnerLogin.class, true, true){
                 @Override
                 protected Intent getActivityIntent (){
                     Intent intent = new Intent();
@@ -50,33 +47,21 @@ public class AccountTest {
      * checks if activity correctly switches when Back button is pressed
      */
     @Test
-    public void checkShareableQrActivitySwitch() {
+    public void checkSuccessfulLogin() {
         // Asserts that the current activity is the Account. Otherwise, show “Wrong Activity”
-        solo.assertCurrentActivity("Wrong Activity", Account.class);
+        solo.assertCurrentActivity("Wrong Activity", OwnerLogin.class);
 
-        // click the back button
-        ImageButton button = (ImageButton) solo.getView(R.id.user_qr_button);
-        solo.clickOnView(button);
+        EditText emailText = (EditText) solo.getView(R.id.email);
+        emailText.setText("gul@ualberta.ca");
+
+        EditText passText = (EditText) solo.getView(R.id.password);
+        passText.setText("qracutie1234");
+
+        Button done = (Button) solo.getView(R.id.owner_login);
+        solo.clickOnView(done);
 
         // Asserts that the current activity switched to ShareableQrActivity. Otherwise, show “Wrong Activity”
-        solo.assertCurrentActivity("Wrong Activity", ShareableQrActivity.class);
-    }
-
-    @Test
-    public void checkDataInput(){
-        EditText emailText = (EditText) solo.getView(R.id.new_email);
-        emailText.setText("testEmail");
-
-        EditText phoneText = (EditText) solo.getView(R.id.new_phonenumber);
-        phoneText.setText("000000000");
-
-        Button done = (Button) solo.getView(R.id.done);
-        solo.clickOnView(done);
-        String newEmail = emailText.getText().toString();
-        String newPhone = phoneText.getText().toString();
-
-        assertEquals("testEmail", newEmail);
-        assertEquals("000000000", newPhone);
+        solo.assertCurrentActivity("Wrong Activity", OwnerAllPlayers.class);
     }
 
     @After
